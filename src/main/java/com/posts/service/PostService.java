01ce63d5e6461961532fa5e80fc42f70765a4659
Yeongung <yun990325@naver.com>
2023-03-03 02:41:15 +0900

    FEAT: PostRepository 추가 FEAT: PostService 추가

---
+++ src/main/java/com/posts/service/PostService.java
@@ -0,0 +1,14 @@
+package com.posts.service;
+
+import com.posts.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class PostService {
+
+    private final PostRepository postRepository;
+}
