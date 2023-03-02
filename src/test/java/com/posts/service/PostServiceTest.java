de1e15462549bf06ad26ae1f35bf62be60dcb62e
Yeongung <yun990325@naver.com>
2023-03-03 03:25:38 +0900

    게시글 단건 조회 테스트코드 추가

--- src/test/java/com/posts/service/PostServiceTest.java
+++ src/test/java/com/posts/service/PostServiceTest.java
@@ -1,8 +1,11 @@
 package com.posts.service;
 
 
+import com.posts.domain.Post;
+import com.posts.exception.NotFoundPostException;
 import com.posts.repository.PostRepository;
 import com.posts.request.PostRequest;
+import com.posts.response.PostDetailResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -41,5 +44,34 @@ class PostServiceTest {
         assertThat(count).isEqualTo(1L);
 
         log.info("postId={}", postId);
+    }
+
+    @Test
+    @DisplayName("게시글 단건 조회")
+    void get() {
+        // given
+        Post post = Post.builder()
+                .username("test username")
+                .title("test title")
+                .content("test content")
+                .build();
+        postRepository.save(post);
+
+        // when
+        PostDetailResponse response = postService.get(post.getId());
+
+        // then
+        assertThat(response.getId()).isEqualTo(1L);
+        assertThat(response.getUsername()).isEqualTo("test username");
+        assertThat(response.getTitle()).isEqualTo("test title");
+        assertThat(response.getContent()).isEqualTo("test content");
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 id 조회로 예외 발생")
+    void getException() {
+        // expected
+        assertThatThrownBy(() -> postService.get(1L))
+                .isInstanceOf(NotFoundPostException.class);
     }
 }
